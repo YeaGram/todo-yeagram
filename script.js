8623cdaac13f823049600f904e493e6588f4e2c5
YeaGram <managerdims@gmail.com>
2022-10-15 15:12:53 +0800

    first Launch

---
+++ script.js
@@ -0,0 +1,15 @@
+// data.forEach((datas) => {
+//   //
+//   const par = document.createElement("li");
+//   item.appendChild(par);
+//   par.className = "list-group-item";
+//   par.innerText += `${datas.kegiatan}`;
+//   //
+// });
+
+// item.forEach((el) => {
+//   console.log(el);
+//   data.forEach((xl) => {
+//     el.innerHTML += ` ${xl.kegiatan} `;
+//   });
+// });
